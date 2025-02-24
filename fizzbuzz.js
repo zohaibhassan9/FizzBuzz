274f1aa092a6c8c9a5861a86679929cfce67c201
zohaibhassan9 <zabaidafashion@gmail.com>
2025-02-24 17:59:34 +0500

    FizzBuzz A Samll mini project

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,13 @@
 // FizzBuzz Game
-for (let i = 1; i < 100; 1++){
+for (let i = 1; i < 100; i++){
 if (i % 3 === 0 && i % 5 ===0 ){
-console.log('FizBuss');
+console.log('FizBuzz');
 }
 else if(i % 3 === 0 ){
 console.log("Fizz");
 }
 else if(i % 5 === 0) {
-console.log("Buzz")
+console.log("Buzz");
 }
 else{
 console.log(i);
