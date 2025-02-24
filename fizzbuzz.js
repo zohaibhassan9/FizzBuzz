16785182e666e7eca2e15775101072f35a45e031
zohaibhassan9 <zabaidafashion@gmail.com>
2025-02-24 17:57:44 +0500

    Syntax, if else block

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,15 @@
 // FizzBuzz Game
+for (let i = 1; i < 100; 1++){
+if (i % 3 === 0 && i % 5 ===0 ){
+console.log('FizBuss');
+}
+else if(i % 3 === 0 ){
+console.log("Fizz");
+}
+else if(i % 5 === 0) {
+console.log("Buzz")
+}
+else{
+console.log(i);
+}
+}
